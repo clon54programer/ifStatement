66679c60f3692fb451e2a63d6c5a73ac9d948fe0
clon54programer <112908720+clon54programer@users.noreply.github.com>
2022-11-15 20:03:55 -0500

    Add project files.

---
+++ ifStatement/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ifStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int anyValue = 16;
+            string message = "";
+
+            if(anyValue <= 7)
+            {
+                message = "omg, it's a miracle any value is 7 ";
+                Console.WriteLine($"{message}{anyValue}");
+            }
+            else if(anyValue ==14){
+                message = "hey, no esxpmple";
+                Console.WriteLine(message);
+            }
+            else if(anyValue <= 15)
+            {
+                Console.WriteLine("hey no bangs");
+            }
+            else
+            {
+                Console.WriteLine("omg, it's not value 7 is "+ anyValue);
+            }
+        }
+    }
+}
